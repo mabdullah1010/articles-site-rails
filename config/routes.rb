4d260bef578cf51619f710b40f07b2957dfb4298
mabdullah1010 <mabdullah@conncoll.edu>
2025-06-08 15:22:25 -0400

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
-
+  resources :articles
 
   get 'articles', to: 'articles#index'
 
